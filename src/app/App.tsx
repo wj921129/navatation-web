c2a2238d3c031ba1f479593c067b60015971bac9
wj921129 <631960490@qq.com>
2026-06-03 22:21:26 +0800

    dev: auto update code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -107,6 +107,7 @@ export default function App() {
     isClockOpen,
     setIsClockOpen,
     isClockClosing,
+    setIsClockClosing,
     isHoveringClock,
     setIsHoveringClock,
     clockTimerRef,
