20d6a7d5e94d7635f56e5194f1c7bb45aef0d606
wj921129 <631960490@qq.com>
2026-06-10 21:44:35 +0800

    清理前端遗留的无用脚本文件

--- src/fix_headers.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-const p = path.join('E:/workspace/navatation/navatation-web/src/app/hooks/useShortcuts.ts');
-let content = fs.readFileSync(p, 'utf-8');
-if (!content.startsWith('/**')) {
-  content = '/**\n * @description 快捷方式管理 Hook\n * @date 2026-06-09\n */\n' + content;
-  fs.writeFileSync(p, content, 'utf-8');
-}
