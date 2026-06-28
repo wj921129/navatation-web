8f7946ddf4ecbd8b965a7ae5f529c61014688e27
wj921129 <631960490@qq.com>
2026-06-28 21:28:47 +0800

    dev: auto update code

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
-
+import viteCompression from 'vite-plugin-compression'
 
 function figmaAssetResolver() {
   return {
@@ -19,6 +18,13 @@ function figmaAssetResolver() {
 
 export default defineConfig({
   plugins: [
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 1024,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
